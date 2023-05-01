6dd00bb314480c4e1bfbff16023924e8d12a5c89
@eundeom <121565744+eundeom@users.noreply.github.com>
2023-05-02 00:31:42 +0900

    image

--- lib/widget/voice2.dart
+++ lib/widget/voice2.dart
@@ -5,12 +5,16 @@ import 'package:flutter_tts/flutter_tts.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart';
+import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:dio/dio.dart';
 
 /* camera button */
 
 class Voice2Screen extends StatefulWidget {
   const Voice2Screen({super.key});
-// hi
+// hihi
   // 상태 관리
   @override
   _VoiceScreen2State createState() => _VoiceScreen2State();
@@ -19,29 +23,47 @@ class Voice2Screen extends StatefulWidget {
 
 class _VoiceScreen2State extends State<Voice2Screen>{
   FlutterTts ftts = FlutterTts();
-  late File _image;
-  final picker = ImagePicker();
+  // late File _image;
+  final ImagePicker picker = ImagePicker();
+  File? _image;
   
   get path => null;
+
+  
+
 
   @override 
   Widget build(BuildContext context) {
     final TextEditingController controller = TextEditingController(text: "안녕하세요");
 
-    Future getImage(ImageSource imageSource) async{
-      final pickedFile = await picker.getImage(source: imageSource);
+    // Future getImage(ImageSource imageSource) async{
+    //   final pickedFile = await picker.getImage(source: imageSource);
 
+    //   setState(() {
+    //     _image = File(pickedFile!.path);
+    //   });
+
+    //   final appDir = await path.provider.getApplicationDocumentsDirectory();
+    //   // final appDir = await getApplicationDocumentsDirectory();
+    //   final fileName = path.basename(pickedFile!.path);
+    //   final savedFile = await _image.copy('${appDir.path}/$fileName');
+    //   String picPath = savedFile.path;
+    // }
+    final picker = ImagePicker();
+    Future getImage(ImageSource source) async {
+      final image = await picker.pickImage(source: source);
+
+      // print("실행중");
+      if (image == null) return;
+      File? img = File(image.path);
+      // Uint8List imagebytes = await image.readAsBytes();
+      // base64Image = base64.encode(imagebytes);
       setState(() {
-        _image = File(pickedFile!.path);
+        _image = img;
+        Navigator.of(context).pop();
+        // print(_image);
       });
-
-      final appDir = await path.provider.getApplicationDocumentsDirectory();
-      // final appDir = await getApplicationDocumentsDirectory();
-      final fileName = path.basename(pickedFile!.path);
-      final savedFile = await _image.copy('${appDir.path}/$fileName');
-      String picPath = savedFile.path;
     }
-
 
     return Positioned(
       child: Container(
@@ -57,8 +79,17 @@ class _VoiceScreen2State extends State<Voice2Screen>{
                   height: 120,
                     child: TextButton(
                       child: const Text(" "),
-                      onPressed: () {
+                      onPressed: () async {
                         getImage(ImageSource.camera);
+
+                        _image == "null"
+                          ? getImage(ImageSource.camera)
+                          : upload( _image);
+                        // if(_image != 'null'){
+                        //   upload( _image);
+                        // }
+                        
+
                       }
                     ),
                   ),
@@ -107,3 +138,29 @@ class _VoiceScreen2State extends State<Voice2Screen>{
       
         
 }
+
+void upload(dynamic base64Image) async {
+    var uploadcase = {
+      // "id" : 3,
+      "image": base64Image,
+      // "braille" : null
+    };
+
+    Dio dio = new Dio();
+    print("실행중");
+    dio.options.headers['content-Type'] = 'application/json';
+    print(uploadcase);
+    try {
+      Response response = await dio.post( // 서버 링크요
+        'https://port-0-backend-5o1j2llh1j01ao.sel4.cloudtype.app/BraiileImg/',
+        data: uploadcase,
+      );//d
+
+      print(response.data);
+      print(response.statusCode);
+    } catch (e) {
+      Exception(e);
+    } finally {
+      dio.close();
+    }
+  }
