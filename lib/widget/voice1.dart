d02f89145478b05c4eda6985dd4de8f2882bc0f2
eundeom <36tfuu@gmail.com>
2023-05-15 00:08:47 +0900

    0514update2

--- lib/widget/voice1.dart
+++ lib/widget/voice1.dart
@@ -5,129 +5,125 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:refresh/refresh.dart';
-import 'package:get/get.dart' as GET;
-// import 'package:bot_toast/bot_toast.dart';
 
 /* 점자 */
 
 class Voice1Screen extends StatefulWidget {
-  const Voice1Screen({super.key});
-  // 상태 관리
+  const Voice1Screen({Key? key}) : super(key: key);
+
   @override
   _VoiceScreen1State createState() => _VoiceScreen1State();
 }
 
-String text1 = ' ';
-
-class _VoiceScreen1State extends State<Voice1Screen>{
+class _VoiceScreen1State extends State<Voice1Screen> {
   FlutterTts ftts = FlutterTts();
   late File _image;
   final picker = ImagePicker();
+  String text1 = '';
+  
 
-  final RefreshController _refreshController = RefreshController(initialRefresh: false);
+  // 사진 가져오기 image picker
+  Future<void> getImage(ImageSource imageSource) async {
+    final pickedFile = await picker.getImage(source: imageSource);
 
-  //새로고침
-  void _onRefresh() async{
-    await Future.delayed(Duration(milliseconds: 4000)); //4초를 기다린 후 새로고침한다.
-    // text field
-    Positioned(
-      top: 20,
-      left: 20,
-      child: SizedBox(
-        width: 350,
-        height: 400,
-        // String textf = text1.substring(6);
-        // String textf = text1.substring(6,11); 6-11까지
-        // String textf = text1.substring(11, str.indexOf(''')); 11부터 '까지
-        child: Text('$text1', //'$text1' or ''+text1
-        style: const TextStyle(
-              fontFamily: 'SF Pro Text', 
-              fontSize: 20, 
-              fontWeight: FontWeight.normal,
-              color: Colors.black),
-        ),
-      ),
-    );
-    _refreshController.refreshCompleted();
+    setState(() {
+      _image = File(pickedFile!.path);
+      // _image == "null"
+      //   ? getImage(ImageSource.camera)
+      //   : upload(_image);
+    });
   }
 
+  Future<void> uploadImageToServer() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
 
+    if (pickedFile != null) {
+      File imageFile = File(pickedFile.path);
 
-  @override 
-  Positioned build(BuildContext context)  {
-    final TextEditingController controller = TextEditingController(text: text1);
-    
-    // 사진 가져오기 image picker
-    Future getImage(ImageSource imageSource) async{
-      final pickedFile = await picker.getImage(source: imageSource);
-
-      setState(() {
-        _image = File(pickedFile!.path);
-        // _image == "null"
-        //   ? getImage(ImageSource.camera)
-        //   : upload(_image);
+      FormData formData = FormData.fromMap({
+        "image": await MultipartFile.fromFile(imageFile.path),
       });
+
+      try {
+        Dio dio = new Dio();
+        dio.options.headers['Content-Type'] = 'application/json';
+        Response response = await dio.post(
+          'https://2d7a-220-122-54-34.ngrok-free.app/api/braille/',
+          data: formData,
+        );
+        text1 = response.toString();
+
+        // text 자르기
+        String textr = text1.substring(9, text1.indexOf("}"));
+        textr = textr.substring(0, textr.length - 1);
+        text1 = textr;
+
+        print(response);
+      } catch (e) {
+        print(e);
+      }
+
+      setState(() {});
     }
-
-
+  }
+  
+  @override
+  Widget build(BuildContext context) {
     return Positioned(
       child: Container(
         child: Stack(
           children: <Widget>[
-            Image.asset(
-              'assets/images/voice2.png'),
-              Positioned(
-                top: 440,
-                left: 45,
-                child: SizedBox(
-                  width: 120,
-                  height: 120,
-                    child: TextButton( // camera button
-                      child: const Text(" "),
-                      onPressed: () async {
-                        // getImage(ImageSource.camera);
-                        setState(() {
-                          uploadImageToServer();
-                          _onRefresh();
-                        });
-                        // text1 = await getTextFromServer();
-                        // print('text : '+text1);
-                      }
-                    ),
-                  ),
-              ),
-
-              // // text field
-              Positioned(
-                top: 20,
-                left: 20,
-                child: SizedBox(
-                  width: 350,
-                  height: 400,
-                  // String textf = text1.substring(6);
-                  // String textf = text1.substring(6,11); 6-11까지
-                  // String textf = text1.substring(11, str.indexOf(''')); 11부터 '까지
-                  child: Text('$text1', //'$text1' or ''+text1
-                  style: const TextStyle(
-                        fontFamily: 'SF Pro Text', 
-                        fontSize: 20, 
-                        fontWeight: FontWeight.normal,
-                        color: Colors.black),
-                  ),
-                ),
-              ),
-
-              // tts
-              Positioned(
-                top: 440,
-                left: 200,
-                child: SizedBox(
+            Image.asset('assets/images/voice2.png'),
+            Positioned(
+              top: 440,
+              left: 45,
+              child: StatefulBuilder(builder: (context, setState) {
+                return SizedBox(
                   width: 120,
                   height: 120,
                   child: TextButton(
+                    // camera button
                     child: const Text(" "),
                     onPressed: () async {
+                      uploadImageToServer();
+                    },
+                  ),
+                );
+              }),
+            ),
+
+            // text field
+            Positioned(
+              top: 20,
+              left: 20,
+              child: SizedBox(
+                width: 350,
+                height: 400,
+                child: Text(
+                  '$text1',
+                  style: const TextStyle(
+                    fontFamily: 'SF Pro Text',
+                    fontSize: 20,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+
+            // tts
+            Positioned(
+              top: 440,
+              left: 200,
+              child: SizedBox(
+                width: 120,
+                height: 120,
+                child: TextButton(
+
+                    child: const Text(" "),
+                    onPressed: () async {
+                      final TextEditingController controller = TextEditingController(text: text1);
                       
                       await ftts.setLanguage("ko-KR"); //en-US ko-KR
                       await ftts.setSpeechRate(0.5); //speed of speech
@@ -152,69 +148,3 @@ class _VoiceScreen1State extends State<Voice1Screen>{
     );
   }
 }
-
-Future<void> uploadImageToServer() async {
-  final picker = ImagePicker();
-  final pickedFile = await picker.getImage(source: ImageSource.gallery);
-
-  if (pickedFile != null) {
-    File imageFile = File(pickedFile.path);
-    // String fileName = imageFile.path.split('/').last;
-
-    FormData formData = FormData.fromMap({
-      "image": await MultipartFile.fromFile(imageFile.path),
-    });
-
-    try {
-      Dio dio = new Dio();
-      dio.options.headers['Content-Type'] = 'application/json';
-      Response response = await dio.post(
-          'https://2d7a-220-122-54-34.ngrok-free.app/api/braille/',
-          data: formData
-      );
-      text1 = response.toString();
-
-      // text 자르기
-      String textr = text1.substring(9,text1.indexOf("}"));
-      textr = textr.substring(0,textr.length-1);
-      text1 = textr;
-
-      print(response);
-    } catch (e) {
-      print(e);
-    }
-    // text1 = await getTextFromServer();
-
-    // String textr = text1.substring(9,text1.indexOf('h'));
-    // text1 = textr;
-    // String textf = splitText(text1) as String;
-
-    // splitText(text1);
-    // print('text : '+text1);
-    // update(text1);
-  }
-}
-
-// void update(String texts) {
-//   text1 = texts;
-// }
-
-// Future<String> getTextFromServer() async {
-//   try {
-//     Dio dio = new Dio();
-//     Response response = await dio.get('https://a8a1-14-45-91-84.ngrok-free.app/Braille/translate');
-//   //  var jsonbody = json.decode(response.data);
-//   //  return jsonbody['answer'];
-//   return response.data.toString();
-//   } catch (e) {
-//     print(e);
-//     return 'error';
-//   }
-// }
-
-// Future<String> splitText(String texts) async {
-//   String textr = texts.substring(10,texts.indexOf(","));
-//   textr = textr.substring(0,textr.length-1);
-//   text1 = textr;
-//   return text1;
-// }
--- lib/widget/voice2.dart
+++ lib/widget/voice2.dart
@@ -5,9 +5,8 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:image_picker/image_picker.dart';
-// import 'package:bot_toast/bot_toast.dart';
 
-/* 묵자 */
+/* 점자 */
 
 class Voice2Screen extends StatefulWidget {
   const Voice2Screen({super.key});
@@ -17,81 +16,117 @@ class Voice2Screen extends StatefulWidget {
   _VoiceScreen2State createState() => _VoiceScreen2State();
 }
 
-String text1 = ' ';
 
 class _VoiceScreen2State extends State<Voice2Screen>{
+
   FlutterTts ftts = FlutterTts();
   late File _image;
   final picker = ImagePicker();
+  String text1 = '';
+  
 
+  // 사진 가져오기 image picker
+  Future<void> getImage(ImageSource imageSource) async {
+    final pickedFile = await picker.getImage(source: imageSource);
 
-  @override 
-  Positioned build(BuildContext context)  {
-    final TextEditingController controller = TextEditingController(text: text1);
-    
+    setState(() {
+      _image = File(pickedFile!.path);
+      // _image == "null"
+      //   ? getImage(ImageSource.camera)
+      //   : upload(_image);
+    });
+  }
 
-    Future getImage(ImageSource imageSource) async{
-      final pickedFile = await picker.getImage(source: imageSource);
+  Future<void> uploadImageToServer() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
 
-      setState(() {
-        _image = File(pickedFile!.path);
-        // _image == "null"
-        //   ? getImage(ImageSource.camera)
-        //   : upload(_image);
+    if (pickedFile != null) {
+      File imageFile = File(pickedFile.path);
+
+      FormData formData = FormData.fromMap({
+        "image": await MultipartFile.fromFile(imageFile.path),
       });
+
+      try {
+        Dio dio = new Dio();
+        dio.options.headers['Content-Type'] = 'application/json';
+        Response response = await dio.post(
+          'https://2d7a-220-122-54-34.ngrok-free.app/api/korean/',
+          data: formData,
+        );
+        text1 = response.toString();
+
+        // text 자르기
+        String textr = text1.substring(9, text1.indexOf("h"));
+        textr = textr.substring(0, textr.length - 1);
+        text1 = textr;
+
+        print(response);
+      } catch (e) {
+        print(e);
+      }
+
+      setState(() {});
     }
-
-
+  }
+  
+  @override
+  Widget build(BuildContext context) {
     return Positioned(
       child: Container(
         child: Stack(
           children: <Widget>[
-            Image.asset(
-              'assets/images/voice1.png'),
-              Positioned(
-                top: 440,
-                left: 45,
-                child: SizedBox(
-                  width: 120,
-                  height: 120,
-                    child: TextButton(
-                      child: const Text(" "),
-                      onPressed: () async {
-                        // getImage(ImageSource.camera);
-                        uploadImageToServer();
-                        
-                        // text1 = await getTextFromServer();
-                        // print('text : '+text1);
-                      }
-                    ),
-                  ),
-              ),
-              // 슬라이드 추가하기
-              Positioned(
-                top: 20,
-                left: 20,
-                child: SizedBox(
-                  width: 350,
-                  height: 400,
-                  child: Text(''+text1,
-                  style: const TextStyle(
-                        fontFamily: 'SF Pro Text', 
-                        fontSize: 20, 
-                        fontWeight: FontWeight.normal,
-                        color: Colors.black),
-                  ),
-                ),
-              ),
-
-              Positioned(
-                top: 440,
-                left: 200,
-                child: SizedBox(
+            Image.asset('assets/images/voice1.png'),
+            Positioned(
+              top: 440,
+              left: 45,
+              child: StatefulBuilder(builder: (context, setState) {
+                return SizedBox(
                   width: 120,
                   height: 120,
                   child: TextButton(
+                    // camera button
                     child: const Text(" "),
                     onPressed: () async {
+                      uploadImageToServer();
+                    },
+                  ),
+                );
+              }),
+            ),
+
+            // text field
+            Positioned(
+              top: 20,
+              left: 20,
+              child: SizedBox(
+                width: 350,
+                height: 400,
+                child: Text(
+                  '$text1',
+                  style: const TextStyle(
+                    fontFamily: 'SF Pro Text',
+                    fontSize: 20,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+
+            // tts
+            Positioned(
+              top: 440,
+              left: 200,
+              child: SizedBox(
+                width: 120,
+                height: 120,
+                child: TextButton(
+
+                    child: const Text(" "),
+                    onPressed: () async {
+                      final TextEditingController controller = TextEditingController(text: text1);
                       
                       await ftts.setLanguage("ko-KR"); //en-US ko-KR
                       await ftts.setSpeechRate(0.5); //speed of speech
@@ -116,69 +151,3 @@ class _VoiceScreen2State extends State<Voice2Screen>{
     );
   }
 }
-
-
-// split func
-// Future<String> splitText(String texts) async {
-//   String textr = texts.substring(10,texts.indexOf(","));
-//   textr = textr.substring(0,textr.length-1);
-//   text1 = textr;
-//   return text1;
-// }
-
-// post
-Future<void> uploadImageToServer() async {
-  final picker = ImagePicker();
-  final pickedFile = await picker.getImage(source: ImageSource.gallery);
-
-  if (pickedFile != null) {
-    File imageFile = File(pickedFile.path);
-    // String fileName = imageFile.path.split('/').last;
-
-    FormData formData = FormData.fromMap({
-      "image": await MultipartFile.fromFile(imageFile.path),
-    });
-
-    try {
-      Dio dio = new Dio();
-      dio.options.headers['Content-Type'] = 'application/json';
-      Response response = await dio.post(
-          'https://2d7a-220-122-54-34.ngrok-free.app/api/korean/',
-          data: formData
-      );
-      text1 = response.toString();
-
-      // text 자르기
-      String textr = text1.substring(9,text1.indexOf("h"));
-      textr = textr.substring(0,textr.length-1);
-      text1 = textr;
-
-      print(response);
-    } catch (e) {
-      print(e);
-    }
-    // text1 = await getTextFromServer();
-
-    // String textr = text1.substring(9,text1.indexOf('h'));
-    // text1 = textr;
-    // String textf = splitText(text1) as String;
-
-    // splitText(text1);
-    // print('text : '+text1);
-  }
-}
-
-
-// get
-// Future<String> getTextFromServer() async {
-//   try {
-//     Dio dio = new Dio();
-//     Response response = await dio.get('https://a2c9-14-45-91-84.ngrok-free.app/Korean/translate');
-//   //  var jsonbody = json.decode(response.data);
-//   //  return jsonbody['answer'];
-//   return response.data.toString();
-//   } catch (e) {
-//     print(e);
-//     return 'error';
-//   }
-// }
